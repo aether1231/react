fe18bbf0c7ec6df47c0e953d98315d1916877855
Kevin <tlk999@live.co.uk>
2022-02-27 15:39:36 +0000

    Section 3 Lecture 35: Adding Basic CSS Styling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
-      <div>March 28th 2021</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>211.21</div>
-      </div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
