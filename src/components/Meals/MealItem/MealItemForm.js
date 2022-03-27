c2990b9a8756439a5e43a1704ab382122686f848
Kevin <tlk999@live.co.uk>
2022-03-27 12:39:45 +0100

    Section 11 Lecture 137: Adding a form

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,8 +1,24 @@
-import Reat from "react";
-import classes from "./MealItemForm.css";
+import React from "react";
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
-  return <div></div>;
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
 };
 
 export default MealItemForm;
